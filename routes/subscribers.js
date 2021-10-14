590a911ae250d46f3d22b11877f9f7bc58a48001
swaff-y <kyle@swaff.id.au>
2021-10-14 21:50:24 +1100

    26:30

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -6,13 +6,13 @@ const Subscriber = require("../models/subscriber")
 
 //Get All
 //need to use the async
+//curl -X GET http://localhost:3000/subscribers/
 router.get("/", async (req,res) => {
-  //res.send("Hello World");
+
   try{
     //this will get all the different subscribers
     const subscribers = await Subscriber.find();
-
-  //  res.json(subscribers)
+    //res.json(subscribers)
     res.status(200).json(subscribers);
   } catch (err) {
     //a 500 error is an error on the database
@@ -23,12 +23,14 @@ router.get("/", async (req,res) => {
 });
 
 //Get One
-router.get("/:id", (req,res) => {
-  //req.params.id
-  res.send(req.params.id)
+//curl -X GET http://localhost:3000/subscribers/6167f42172f1a407b2bcbeef
+//The "getSubscriber" middleware will get the subscriber with the id
+router.get("/:id", getSubscriber, (req,res) => {
+  res.status(200).json(res.subscriber);
 });
 
 //Create one
+//curl -X POST -H "Content-Type: application/json" -d '{"name":"Kyle","subscribedToChannel":"Swaff-y"}' http://localhost:3000/subscribers/
 router.post("/", async (req,res) => {
   //get subscriber details from the json body
   const subscriber = new Subscriber({
@@ -44,18 +46,45 @@ router.post("/", async (req,res) => {
   } catch (err) {
     //when user give bad data send 400 because there is something wrong with the request.
     res.status(400).json({ message: err.message });
-    console.log("Error", req.body);
   };
 });
 //Update One
 //put updates all. Patch only updates the info passed
-router.patch("/:id", (req,res) => {
+//curl -X PATCH http://localhost:3000/subscribers/6167f42172f1a407b2bcbeef
+router.patch("/:id", getSubscriber, (req,res) => {
 
 });
 //Delete One
-router.delete("/:id", (req,res) => {
-
+//curl -X DELETE http://localhost:3000/subscribers/6167f42172f1a407b2bcbeef
+router.delete("/:id", getSubscriber, async (req,res) => {
+  try{
+    //try remove the subscriber
+    await res.subscriber.remove();
+    res.status(201).json({ message: "Deleted Subscriber " + res.subscriber.name});
+  } catch (err) {
+    //else catch the error
+    res.status(500).json({ message: err.message });
+  }
 });
+
+//middleware function to get subscriber
+async function getSubscriber(req, res, next){
+  let subscriber;
+  try{
+    subscriber = await Subscriber.findById(req.params.id);
+    if(subscriber === null){
+      //404 status means you could not find something
+      return res.status(404).json({ message: "Could not find subscriber with id " + req.params.id})
+    }
+  } catch(err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  //set response to be equal to subscriber
+  res.subscriber = subscriber;
+  // call next to move onto the next piece of middleware or on to the actual request itself
+  next()
+}
 
 //exports our routes
 module.exports = router
